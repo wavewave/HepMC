403c4ab66c359c93e53f69b5fd5b980bc8a4f3f4
Ian-Woo Kim <ianwookim@gmail.com>
2014-06-03 11:37:18 +0000

    more correct definition of HepMC type

--- src/HEP/Parser/HepMC.hs
+++ src/HEP/Parser/HepMC.hs
@@ -1,16 +1,30 @@
 module HEP.Parser.HepMC where
 
--- data BlockDirective = Begin 
---                     | End
+type Version = String
 
-data Line = E GenEvent 
+data EventBlock = EventBlock Version [ Event ]
+
+data MomentumUnit = GeV | MeV 
+                  deriving (Show)
+
+data LengthUnit = MM | CM
+                deriving (Show)
+
+{- data Line = E GenEvent 
           | N NamedWeight
-          | U MomentumPositionUnits
+          | U MomentumPositionUnit
           | C GenXSec
           | H HeavyIon
           | F PdfInfo
           | V GenVertex
           | P GenParticle
+-}
+
+data Event = Event { genEventInfo :: GenEvent
+                   , eventHeader :: EventHeader 
+                   , vertices :: [GenVertex] 
+                   }
+           deriving Show
 
 data GenEvent = GenEvent { eventNumber :: Int
                          , numMultiparticleInteractions :: Int
@@ -22,17 +36,28 @@ data GenEvent = GenEvent { eventNumber :: Int
                          , numVtx :: Int
                          , barcodeBeam1 :: Int
                          , barcodeBeam2 :: Int
-                         , optionalRandomStateIntegers :: Int
-                         , optionalWeight :: Int }
+                         , randomStateList :: (Int, [Int]) -- ^ (numEntries, randomStateIntergers)
+                         , weightList :: (Int, [Double])  -- ^ (numEntries, weights )
+                         }
               deriving (Show) 
 
+data EventHeader = EventHeader { weightInfo :: NamedWeight
+                               , unitInfo :: MomentumPositionUnit
+                               , xsecInfo :: Maybe GenXSec 
+                               , heavyIonInfo :: HeavyIon
+                               , pdfInfo :: PdfInfo
+                               }
+                 deriving (Show)
+            
 data NamedWeight = NamedWeight { numEntries :: Int
-                               , weightNames :: String }
+                               , weightNames :: [ String ] 
+                               }
                  deriving (Show)
 
-data MomentumPositionUnits = MomentumPositionUnits { momentumUnits :: String
-                                                   , lengthUnits :: String }
-                           deriving (Show)
+data MomentumPositionUnit = MomentumPositionUnit { momentumUnit :: MomentumUnit
+                                                 , lengthUnit :: LengthUnit 
+                                                 }
+                          deriving (Show)
 
 data GenXSec = GenXSec { xsecInPb :: Double 
                        , errorInXsec :: Double }
@@ -72,8 +97,9 @@ data GenVertex = GenVertex { vbarcode :: Int
                            , vctau :: Double
                            , numOrphanInPtl :: Int
                            , numOutPtl :: Int
-                           , numEntriesInWeightList :: Int
-                           , optionalWeights :: Double } 
+                           , vertexWeights :: (Int, [Double])  -- ^ ( numEntries, weights ) 
+                           , particles :: [GenParticle]
+                           } 
                deriving (Show)
 
 data GenParticle = GenParticle { pbarcode :: Int
@@ -86,8 +112,8 @@ data GenParticle = GenParticle { pbarcode :: Int
                                , statusCode :: Int
                                , polTheta :: Double 
                                , polPhi :: Double
-                               , vbarcodeAsIncoming :: Int
-                               , numEntriesFlow :: Int
-                               , optionalCodeIndexNCode :: (Int,Int) }
+                               , vbarcode4ThisIncoming :: Int
+                               , flows :: (Int, [(Int,Int)] ) -- ^ (numEntries, [ (code index, code) ] )
+                               }
                  deriving (Show)
 
