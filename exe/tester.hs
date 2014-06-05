cdb24973e9ad23760a0ec541dafe85fd6e60329c
Ian-Woo Kim <ianwookim@gmail.com>
2014-06-05 19:51:48 +0000

    some experiments with pipes

--- exe/tester.hs
+++ exe/tester.hs
@@ -4,10 +4,11 @@ import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Class
 import           Control.Monad.Trans.State.Strict
 import qualified Data.Attoparsec.Text as A
-import           Data.Text
+import           Data.Text 
 import qualified Data.Text.IO as TIO
 -- import           Pipes (await, runEffect, yield, (>->))
 import           Pipes
+import qualified Pipes.Prelude
 import qualified Pipes.Attoparsec as PA
 import qualified Pipes.Parse as PP
 import qualified Pipes.Text.IO as PIO
@@ -15,42 +16,30 @@ import System.Environment
 import System.IO (withFile, IOMode(..),  stdout)
 -- 
 import HEP.Parser.HepMC
+--
 
-
-{- 
-main = do 
-  putStrLn "hepmc tester" 
-  args <- getArgs 
-  let filename = args !! 0
-  str <- TIO.readFile filename 
-  print (A.parse hepmcHeader str)
--}
+takeWait :: (Monad m) => Int -> Pipe a a m ()
+takeWait n = worker 0 
+  where worker m 
+          | m > n = await >> worker (m+1)  
+          | otherwise = await >>= yield >> worker (m+1)        
 
 main = do 
   putStrLn "hepmc tester" 
   args <- getArgs 
   let filename = args !! 0
       p = PA.parse hepmcHeader
-      -- evp = PA.parse event
   print filename 
   withFile filename ReadMode $ \hin -> 
     runEffect $ do
       (r1,s) <- lift (runStateT p (PIO.fromHandle hin))
-      forever (action s) >-> printpipe 0
-      -- r2 <- evalStateT evp s 
-      -- liftIO $ print (r1,r2)
-      -- p' >-> lift (PIO.toHandle stdout)
-  -- print x 
-  -- str <- TIO.readFile filename 
-  -- print (parse hepmc str)
+      forever (action s) >-> Pipes.Prelude.tee (counter 100 0 >-> forever (await >> return ())) 
+        >-> takeWait 500 >-> counter 70 0 >-> (replicateM 3 printer >> forever (await >> return ())) -- printer
   return ()
--- flip runStateT (PIO.fromHandle hin) $ (p >> forever action) >-> return ()
-
 
 evp :: (Monad m) => PP.Parser Text m (Maybe (Either PA.ParsingError (Text, [Int])))
 evp = PA.parse event
 
--- action :: Producer Text (Producer Double m) r
 action :: (Monad m) => Producer Text m () -> Producer (Text, [Int]) m () 
 action s = do
   (r,s') <- lift (runStateT evp s)
@@ -60,11 +49,21 @@ action s = do
     Just (Right ev) -> yield ev >> action s'
 
  
+counter :: (MonadIO m, Monad m) => Int -> Int -> Pipe a a m () 
+counter m n = do 
+  e <- await
+  if n `mod` m == 0 then liftIO (print n) else return ()  
+  yield e 
+  counter m (n+1)
 
-printpipe :: (MonadIO m, Monad m) => Int -> Consumer (Text, [Int]) m ()
-printpipe n = do 
-  ev <- await
-  if n `mod` 100 == 0 then liftIO (print n) else return ()  
-  -- liftIO $ print ev 
-  printpipe (n+1)
+
+printer :: (MonadIO m, Monad m, Show a) => Consumer a m () 
+printer = do
+  e <- await 
+  liftIO (print e)
+
+
+
+
+-- Consumer (Text, [Int]) m ()
 
