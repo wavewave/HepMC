38d7840fe93e3812b5e916d30f5ee46477b25d54
Ian-Woo Kim <ianwookim@gmail.com>
2014-09-24 18:23:43 +0000

    make generic generation : so electron, muon cases are treated together

--- test.hs
+++ test.hs
@@ -30,13 +30,23 @@ p1 = GenParticle { pbarcode = 1
                  , vbarcode4ThisIncoming = -1
                  , flows = (0,[]) }
                                                     
-genElectron :: MTGen -> Double -> Double -> IO B.ByteString
-genElectron gen pt eta = do
+genEvent :: (Int -> (Double,Double, Double) -> GenEvent) 
+            ->  MTGen -> Double -> Double -> IO B.ByteString
+genEvent evgen gen pt eta = do
     -- phi <- (2.0 * pi *) <$> random gen :: IO Double    
     plst <- sequence (replicate 10000 (go gen pt eta))
-    let evts = zipWith testevent [1..] plst
+    let evts = zipWith evgen [1..] plst
     return $ toByteString (buildHepMC evts)
+{- 
+genMuon :: MTGen -> Double -> Double -> IO B.ByteString
+genMuon gen pt eta = do
+    -- phi <- (2.0 * pi *) <$> random gen :: IO Double    
+    plst <- sequence (replicate 10000 (go gen pt eta))
+    let evts = zipWith elect [1..] plst
+    return $ toByteString (buildHepMC evts)
+-}
 
+go :: MTGen -> Double -> Double -> IO (Double,Double,Double)
 go gen pt eta = do
     phi <- (2.0 * pi *) <$> random gen :: IO Double
     
@@ -49,35 +59,48 @@ go gen pt eta = do
     return (px,py,pz)  
 
 
-mkFilename :: Double -> Double -> FilePath
-mkFilename pt eta = "electron_" ++ show pt ++ "_" ++ show eta ++ ".hepmc"
+mkFilename :: String -> Double -> Double -> FilePath
+mkFilename str pt eta = str ++ "_" ++ show pt ++ "_" ++ show eta ++ ".hepmc"
 
 ptetaset = [ (pt,eta) | pt <- [ 10,20,30,40,50,60,70,80,90,100 ] ++ [150,200,250,300] , eta <- [-3.0,-2.5..3.0 ] ]
  
-work gen (pt,eta) = do
-    let filename = mkFilename pt eta 
-    B.writeFile filename =<< genElectron gen pt eta
+work str evgen gen (pt,eta) = do
+    let filename = mkFilename str pt eta 
+    B.writeFile filename =<< genEvent evgen gen pt eta
 
+main' = do 
+  gen <- newMTGen Nothing
+  mapM_  (work "electron" mkelec gen) ptetaset 
 
 main = do 
   gen <- newMTGen Nothing
+  mapM_  (work "muon" mkmuon gen) ptetaset 
 
-  mapM_  (work gen) ptetaset 
 
-testevent n (px,py,pz) = GenEvent { eventNumber = n 
-                     , numMultiparticleInteractions = 1
-                     , eventScale = 0
-                     , alphaQCD = 0
-                     , alphaQED = 0
-                     , signalProcessId = 0 
-                     , barcode4SignalProcessVtx= -3  
-                     , numVtx = 3
-                     , barcodeBeam1 = 1
-                     , barcodeBeam2 = 4
-                     , randomStateList = (0, []) 
-                     , weightList = (0, [] )
-                     , eventHeader = testheader
-                     , vertices = testvertices (px,py,pz) }
+dummybody :: (Int,Int) -> Int -> (Double,Double,Double) 
+          -> ((Int,Int) -> (Double,Double,Double) -> [GenVertex]) -> GenEvent
+dummybody (pdg1,pdg2) n (px,py,pz) genvtx = 
+    GenEvent { eventNumber = n 
+             , numMultiparticleInteractions = 1
+             , eventScale = 0
+             , alphaQCD = 0
+             , alphaQED = 0
+             , signalProcessId = 0 
+             , barcode4SignalProcessVtx= -3  
+             , numVtx = 3
+             , barcodeBeam1 = 1
+             , barcodeBeam2 = 4
+             , randomStateList = (0, []) 
+             , weightList = (0, [] )
+             , eventHeader = testheader
+             , vertices = genvtx (pdg1,pdg2) (px,py,pz) }
+
+
+mkelec :: Int -> (Double,Double,Double) -> GenEvent
+mkelec n (px,py,pz) = dummybody (11,-12) n (px,py,pz) testvertices
+ 
+mkmuon :: Int -> (Double,Double,Double) -> GenEvent
+mkmuon n (px,py,pz) = dummybody (13,-14) n (px,py,pz) testvertices
                      
 testheader = EventHeader { mWeightInfo = Nothing
                          , mUnitInfo = Just (MomentumPositionUnit GeV MM)
@@ -85,7 +108,7 @@ testheader = EventHeader { mWeightInfo = Nothing
                          , mHeavyIonInfo = Nothing
                          , mPdfInfo = Nothing }
 
-testvertices (px,py,pz) = [ testvertex1, testvertex2, testvertex3 (px,py,pz) ]
+testvertices (pdg1,pdg2) (px,py,pz) = [ testvertex1, testvertex2, testvertex3 (pdg1,pdg2) (px,py,pz) ]
 
 testvertex1 = GenVertex { vbarcode = -1
                         , vid = 0
@@ -191,12 +214,12 @@ testvertex2 = GenVertex { vbarcode = -2
                                       ]
                             }
 
-testvertex3 (px',py',pz') = GenVertex { vbarcode = -3
+testvertex3 (pdg1,pdg2) (px',py',pz') = GenVertex { vbarcode = -3
                         , vid = 0, vx = 0, vy = 0, vz = 0, vctau = 0 
                         , numOrphanInPtl = 0, numOutPtl = 2
                         , vertexWeights = (0,[])
                         , particles = [ GenParticle { pbarcode = 7
-                                                    , pidPDG = 11
+                                                    , pidPDG = pdg1
                                                     , px = px', py = py', pz = pz', pE = sqrt (px'^2 + py'^2 + pz'^2), generatedMass = 0.0
                                                     , statusCode = 1
                                                     , polTheta = 0, polPhi = 0 
@@ -204,7 +227,7 @@ testvertex3 (px',py',pz') = GenVertex { vbarcode = -3
                                                     , flows = (0,[])
                                                     }
                                       , GenParticle { pbarcode = 8
-                                                    , pidPDG = -12
+                                                    , pidPDG = pdg2
                                                     , px = -px', py = -py', pz = -pz', pE = sqrt (px'^2 + py'^2 + pz'^2), generatedMass = 0.0
                                                     , statusCode = 1
                                                     , polTheta = 0, polPhi = 0 
